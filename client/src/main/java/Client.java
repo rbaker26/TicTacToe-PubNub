f56697bd516c76e430aab1e8cb15757d7db8032a
Autofire <Autofire@users.noreply.github.com>
2019-03-29 13:17:17 -0700

    Made it so that we can send messages to ourselves.

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -8,26 +8,31 @@ import com.pubnub.api.models.consumer.PNPublishResult;
 import com.pubnub.api.models.consumer.PNStatus;
 import com.pubnub.api.models.consumer.pubsub.PNMessageResult;
 import com.pubnub.api.models.consumer.pubsub.PNPresenceEventResult;
+import java.util.concurrent.TimeUnit;
 
 import java.util.Arrays;
 
-
 public class Client {
+
 
     public static void main(String[] args) {
         PNConfiguration pnConfiguration = new PNConfiguration();
-        pnConfiguration.setSubscribeKey("demo");
-        pnConfiguration.setPublishKey("demo");
+        pnConfiguration.setSubscribeKey("sub-c-89b1d65a-4f40-11e9-bc3e-aabf89950afa");
+        pnConfiguration.setPublishKey("pub-c-79140f4c-8b9e-49ed-992f-cf6322c68d04");
 
         PubNub pubnub = new PubNub(pnConfiguration);
 
         String channelName = "awesomeChannel";
 
         // create message payload using Gson
-        JsonObject messageJsonObject = new JsonObject();
-        messageJsonObject.addProperty("msg", "hello");
+        MoveData data = new MoveData();
+        data.column = 1;
+        data.row = 30;
+        //JsonObject messageJsonObject = new JsonObject();
+        //messageJsonObject.addProperty("msg", "hello");
 
-        System.out.println("Message to send: " + messageJsonObject.toString());
+        //System.out.println("Message to send: " + messageJsonObject.toString());
+        System.out.println("Message to send: " + data.toString());
 
         pubnub.addListener(new SubscribeCallback() {
             @Override
@@ -45,7 +50,7 @@ public class Client {
                     // UI / internal notifications, etc
 
                     if (status.getCategory() == PNStatusCategory.PNConnectedCategory){
-                        pubnub.publish().channel(channelName).message(messageJsonObject).async(new PNCallback<PNPublishResult>() {
+                        pubnub.publish().channel(channelName).message(data).async(new PNCallback<PNPublishResult>() {
                             @Override
                             public void onResponse(PNPublishResult result, PNStatus status) {
                                 // Check whether request successfully completed or not.
@@ -89,11 +94,34 @@ public class Client {
                     // message.getSubscription()
                 }
 
-                JsonElement receivedMessageObject = message.getMessage();
-                System.out.println("Received message content: " + receivedMessageObject.toString());
-                // extract desired parts of the payload, using Gson
-                String msg = message.getMessage().getAsJsonObject().get("msg").getAsString();
-                System.out.println("msg content: " + msg);
+                System.out.println();
+                System.out.println("msg content: " + message.getMessage());
+                System.out.println("msg publisher: " + message.getPublisher());
+
+                GsonBuilder builder = new GsonBuilder();
+                Gson gson = builder.create();
+                MoveData origMove = gson.fromJson(message.getMessage(), MoveData.class);
+
+                System.out.println( "Move class: " + origMove.toString() );
+
+                // We want to wait a bit after getting a message.
+                // Otherwise, we could burn up all of our messages and make pubnub unhappy.
+                try {
+                    TimeUnit.SECONDS.sleep(3);
+                }
+                catch(InterruptedException ex) {
+
+                }
+
+                MoveData responseMove = new MoveData();
+                responseMove.column = origMove.column + 1;
+                responseMove.row = origMove.row - 1;
+
+                pubnub.publish().channel(channelName).message(responseMove).async(new PNCallback<PNPublishResult>() {
+                    @Override
+                    public void onResponse(PNPublishResult result, PNStatus status) {
+                    }
+                });
 
 
             /*
---
+++ client/src/main/java/MoveData.java
@@ -0,0 +1,12 @@
+public class MoveData {
+    public int row;
+    public int column;
+
+    @Override
+    public String toString() {
+        return "MoveData{" +
+                "row=" + row +
+                ", column=" + column +
+                '}';
+    }
+}
