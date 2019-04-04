ed084d026191b0cb45b172b449af581a0104cc93
kkaiser87 <kaiser.keane@gmail.com>
2019-04-04 11:28:03 -0700

    Added dependencies for the modules

---
+++ game_engine/src/main/java/Engine.java
@@ -0,0 +1,3 @@
+public class Engine {
+    Lobby lobby;
+}
---
+++ library/src/main/java/Lobby.java
@@ -0,0 +1,10 @@
+public class Lobby {
+    private int roomId;
+    private int p1Id;
+    private int p2Id;
+    private String roomStatus;
+
+    public Lobby() {
+
+    }
+}
