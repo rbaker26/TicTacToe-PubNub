35c69256abcd7e3d0a173c5250e1681cbc0eccb7
Bob Baker <rbaker26@saddleback.edu>
2019-04-09 09:21:43 -0700

    fixed issues

---
+++ recorder/src/main/java/RecorderServer.java
@@ -0,0 +1,20 @@
+public class RecorderServer {
+
+    public static void main(String[] args){
+        Db_Manager db = Db_Manager.GetInstance();
+
+
+
+//        Move move = new Move(1,2,3,4);
+//        System.out.println(move);
+//
+        try {
+            db.WriteMove(99553, 1, 1, 6587);
+        }
+        catch(Exception e){
+            System.out.println(e.toString());
+        }
+    }
+
+
+}
