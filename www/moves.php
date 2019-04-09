515ab65b973f2afa9d7c6148c6875fc33c6b26de
Bob Baker <31376573+rbaker26@users.noreply.github.com>
2019-04-09 12:13:07 -0700

    ajax update on move.php

--- www/moves.php
+++ www/moves.php
@@ -2,8 +2,11 @@
 <html>
 <head>
     <?php include 'head.php'; ?>
+    <script src="moveUpdate.js"></script>
+
 </head>
 <body>
+<div onmouseover="reload_table()" onmouseout="reload_table()" onclick="reload_table()">
 
     <?php include './header.php';?>
     <br/>
@@ -11,7 +14,8 @@
     <h1 align="center">Move History View</h1>
     <br/>
     <br/>
-    <table id="moves">
+    <h1 id="test"> </h1>
+    <table id="moves" onmouseover="reload_table()" onmouseout="reload_table()" onclick="reload_table()">
         <tr>
             <!-- <th>Primary Key</th> -->
             <th>Room ID</th> 
@@ -20,7 +24,8 @@
             <th>Player ID</th>
         </tr>
         <?php include './getMovesTable.php'; ?>
+          
     </table>
-
+<div>
 </body>
 </html>
