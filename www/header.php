a197ea7f706052bcee55ac3a57f6fb1899c71d77
Bob Baker <rbaker26@saddleback.edu>
2019-04-09 01:22:17 -0700

    switch to php, works well

---
+++ www/header.php
@@ -0,0 +1,2 @@
+<h1 align="center">TicTacToe Database Concept Viewer</h1>
+<?php include './nav.php'?>
---
+++ www/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include 'head.php'; ?>
+</head>
+<body>
+    <?php include './header.php';?>
+
+</body>
+</html>
