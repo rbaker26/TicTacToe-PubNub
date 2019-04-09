a9fc22e0f6f228635f5c9a3de52ebbc82f6ec1ff
Bob Baker <31376573+rbaker26@users.noreply.github.com>
2019-04-09 12:35:50 -0700

    ajax update on move.php

--- recorder/src/main/java/RecorderServer.java
+++ recorder/src/main/java/RecorderServer.java
@@ -11,6 +11,15 @@ public class RecorderServer {
         try {
             //db.WriteMove(99553, 1, 1, 6587);
             //db.WritePlayer("Naomi", 88888);
+
+
+
+            for(int i = 0; i < 3; i++) {
+                for(int j = 0; j < 3; j++) {
+                    db.WriteMove(10000, i, j, 4653);
+                 //   Thread.sleep(10);
+                }
+            }
         }
         catch(Exception e){
             System.out.println(e.toString());
