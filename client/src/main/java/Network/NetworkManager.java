cb8f4764dc0b039442644a80191368bdf4d926b1
Autofire <Autofire@users.noreply.github.com>
2019-04-15 18:50:18 -0700

    Added comments and refactored a few NetworkManager things.

--- client/src/main/java/Network/NetworkManager.java
+++ client/src/main/java/Network/NetworkManager.java
@@ -9,6 +9,7 @@ import com.pubnub.api.callbacks.PNCallback;
 import com.pubnub.api.callbacks.SubscribeCallback;
 import com.pubnub.api.models.consumer.PNPublishResult;
 import com.pubnub.api.models.consumer.PNStatus;
+import org.w3c.dom.ranges.RangeException;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -22,6 +23,11 @@ public final class NetworkManager {
     private static String uuidModifier = "";
 
     private static NetworkManager _instance;
+
+    /**
+     * Gets the instance of the network manager. Creates it if necessary.
+     * @return The network manager.
+     */
     public static NetworkManager getInstance() {
         if(_instance == null) {
             _instance = new NetworkManager();
@@ -30,6 +36,12 @@ public final class NetworkManager {
         return _instance;
     }
 
+    /**
+     * Sets the uuid modifier. This is mainly intended for debug purposes,
+     * such as when we want to run multiple instances of the client on
+     * a single computer.
+     * @param uuidModifier The string to append to the UUID.
+     */
     public static void setUuidModifier(String uuidModifier) {
         NetworkManager.uuidModifier = uuidModifier;
     }
@@ -73,11 +85,25 @@ public final class NetworkManager {
     }
     //endregion
 
+
+
     private PubNub pn;
     private SubscribeCallback currentListener = null;
     private List<String> currentChannels = null;
 
+    /**
+     * Changes to another listener. This will override whatever current listener we have.
+     * @param newListener The callback which will listen on the channels.
+     * @param newChannels The channels to subscribe to.
+     * @throws IllegalArgumentException Thrown if newListener is null and newChannels is not, or if newChannels is null
+     *                                  and newListener is not.
+     */
     private void changeListener(SubscribeCallback newListener, List<String> newChannels) {
+
+        if(newListener == null ^ newChannels == null) {
+            throw new IllegalArgumentException("Either both newListener and newChannels must not be null, or they must both be null.");
+        }
+
         // To reduce slowdown, we'll swap in our new listener,
         // subscribe to the new channel, and then unsubscribe
         // from the old channel.
@@ -105,13 +131,29 @@ public final class NetworkManager {
         currentChannels = newChannels;
     }
 
+    /**
+     * Removes current listener and unsubscribes from all channels.
+     */
+    private void removeListener() {
+        changeListener(null, null);
+    }
+
+    /**
+     * Starts listening for rooms.
+     * @param onUpdateCallback Gets fired whenever there is a new room list.
+     */
     public void listenForRooms(Consumer<List<RoomInfo>> onUpdateCallback) {
-        // TODO Ping the engine to update the room list
-        // TODO Have a callback to let the outside world know
 
         changeListener(new RoomListCallback(onUpdateCallback), Arrays.asList(Channels.roomListChannel));
     }
 
+    /**
+     * Asks the engine for a new room.
+     * @param userID Our ID.
+     * @param goingFirst If true, we'll be going first.
+     * @param successResponse Is called upon successfully getting a room. If null, the listeners will be cleared.
+     * @param failureResponse Is called upon unsuccessfully getting a room. If null, the listeners will be cleared.
+     */
     public void requestNewRoom(String userID, boolean goingFirst, Consumer<RoomInfo> successResponse, Consumer<RoomInfo> failureResponse) {
 
         //clearCurrentListener();
@@ -128,29 +170,43 @@ public final class NetworkManager {
                 userID, outgoingChannel, incomingChannel, roomInfo
         );
 
-        callback.setSuccessResponse(responseRoomInfo -> {
-            if(successResponse != null) {
-                successResponse.accept(responseRoomInfo);
-            }
 
-            changeListener(null, null);
-        });
-        callback.setFailureResponse(responseRoomInfo -> {
-            if(failureResponse != null) {
-                failureResponse.accept(responseRoomInfo);
-            }
+        if(successResponse == null) {
+            successResponse = (info) -> {
+                removeListener();
+            };
+        }
+        if(failureResponse == null) {
+            failureResponse = (info) -> {
+                removeListener();
+            };
+        }
 
-            changeListener(null, null);
-        });
+        callback.setSuccessResponse(successResponse);
+        callback.setFailureResponse(failureResponse);
+
 
         changeListener(callback, Arrays.asList(incomingChannel));
     }
 
     /**
-     * Joins a room which has been created via request requestNewRoom.
+     * Call the other joinRoom function instead.
+     * @param ourUserID
      * @param roomInfo
      */
+    @Deprecated
     public void joinRoom(String ourUserID, RoomInfo roomInfo) {
+        joinRoom(ourUserID, roomInfo, null, null);
+    }
+
+    /**
+     * Joins a room.
+     * @param ourUserID Our user ID.
+     * @param roomInfo Info on the room. (Probably should have come as a result of listenForRooms.
+     * @param successResponse Called upon successfully joining the room. If null, the listeners will be cleared.
+     * @param failureResponse Called upon unsuccessfully joining the room. If null, the listeners will be cleared.
+     */
+    public void joinRoom(String ourUserID, RoomInfo roomInfo, Consumer<RoomInfo> successResponse, Consumer<RoomInfo> failureResponse) {
         String incomingChannel = Channels.privateChannelSet + roomInfo.getPlayer1ID();
         String outgoingChannel = Channels.roomRequestChannel;
 
@@ -164,14 +220,21 @@ public final class NetworkManager {
                 ourUserID, outgoingChannel, incomingChannel, roomInfo
         );
 
-        callback.setSuccessResponse(responseRoomInfo -> {
-            changeListener(null, null);
-        });
 
-        /*
-        pn.addListener(callback);
-        pn.subscribe().channels(Arrays.asList(incomingChannel)).execute();
-         */
+        if(successResponse == null) {
+            successResponse = (info) -> {
+                removeListener();
+            };
+        }
+        if(failureResponse == null) {
+            failureResponse = (info) -> {
+                removeListener();
+            };
+        }
+
+        callback.setSuccessResponse(successResponse);
+        callback.setFailureResponse(failureResponse);
+
 
         changeListener(callback, Arrays.asList(incomingChannel));
     }
