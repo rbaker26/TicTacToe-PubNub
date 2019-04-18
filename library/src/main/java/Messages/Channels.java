24f6d9cd1436b612d1d732264511eca26d55f71a
Autofire <Autofire@users.noreply.github.com>
2019-04-18 15:44:10 -0700

    Fixed the channel names. They now use dots instead of colons.

--- library/src/main/java/Messages/Channels.java
+++ library/src/main/java/Messages/Channels.java
@@ -3,9 +3,9 @@ package Messages;
 public final class Channels {
     private Channels() {}
 
-    public static final String roomChannelSet = "Rooms::";
-    public static final String privateChannelSet = "Private::";
-    public static final String heartbeatChannelSet = "Heartbeat::";
+    public static final String roomChannelSet = "Rooms.";
+    public static final String privateChannelSet = "Private.";
+    public static final String heartbeatChannelSet = "Heartbeat.";
 
     public static final String roomRequestChannel = roomChannelSet + "Requests";
     public static final String roomListChannel = roomChannelSet + "List";
