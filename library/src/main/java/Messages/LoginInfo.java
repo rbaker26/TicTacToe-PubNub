0d95fb3ecd5c087f2e51dadfcde812170ce15416
kkaiser87 <kaiser.keane@gmail.com>
2019-04-24 10:58:00 -0700

    LoginInfo added

--- library/src/main/java/Messages/LoginInfo.java
+++ library/src/main/java/Messages/LoginInfo.java
@@ -1,4 +1,45 @@
 package Messages;
 
 public class LoginInfo {
+
+    private String username;
+    private String password;
+    private String screenName;
+
+
+    public LoginInfo(String username, String password, String screenName) {
+
+        this.username = username;
+        this.password = password;
+        this.screenName = screenName;
+
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getScreenName()
+    {
+        return screenName;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setScreenName(String screenName) {
+        this.screenName = screenName;
+    }
+
+
+
+
 }
